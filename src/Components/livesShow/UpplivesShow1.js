a82caaff438faedb5b52273f8312619958f36234
ahmedsa11 <ahmedsa122333@gmail.com>
2024-06-28 18:14:12 +0300

    e

--- src/Components/livesShow/UpplivesShow1.js
+++ src/Components/livesShow/UpplivesShow1.js
@@ -55,51 +55,54 @@ const UppliveShow1 = () => {
       <div id="local_stream"></div>
 
       {res &&
-        res.map((item) => (
-          <div className="sec">
-            <Link
-              to={`/upliveDetails/${item._id}`}
-              style={{ textDecoration: "none", color: "black" }}>
-              <div className="p-3 text-dark-emphasis bg-secondary-subtle border border-dark-subtle rounded-3 con">
-                {item.hostIds.map((ho) => (
-                  <div className="con1">
-                    <div className="img">
-                      <img src={userImg} alt="user" class="user-img" />
+        res.map((item) =>
+          item.event === false && item.ended === false ? (
+            <div className="sec">
+              <Link
+                to={`/upliveDetails/${item._id}`}
+                style={{ textDecoration: "none", color: "black" }}>
+                <div className="p-3 text-dark-emphasis bg-secondary-subtle border border-dark-subtle rounded-3 con">
+                  {item.hostIds.map((ho) => (
+                    <div className="con1">
+                      <div className="img">
+                        <img src={userImg} alt="user" class="user-img" />
+                      </div>
+                      <div className="info">
+                        <p>{ho.name}</p>
+                        <span>Host</span>
+                      </div>
                     </div>
-                    <div className="info">
-                      <p>{ho.name}</p>
-                      <span>Host</span>
+                  ))}
+                  {item.userIds.map((us) => (
+                    <div className="con1">
+                      <div className="img">
+                        <img src={userImg} alt="user" class="user-img" />
+                      </div>
+                      <div className="info">
+                        <p>{us.name}</p>
+                        <span>user</span>
+                      </div>
                     </div>
-                  </div>
-                ))}
-                {item.userIds.map((us) => (
-                  <div className="con1">
-                    <div className="img">
-                      <img src={userImg} alt="user" class="user-img" />
-                    </div>
-                    <div className="info">
-                      <p>{us.name}</p>
-                      <span>user</span>
-                    </div>
-                  </div>
-                ))}
+                  ))}
 
-                <div className="micro">
-                  <img src={recored} className="recored-icon" alt="recored" />
+                  <div className="micro">
+                    <img src={recored} className="recored-icon" alt="recored" />
+                  </div>
                 </div>
-              </div>
-            </Link>
-            <div className="para3">
-              <p className="p1" style={{ color: "#333" }}>
-                {item.title}
-              </p>
-              <p className="p2">{new Date(item.eventDate).toLocaleString()}</p>
-              <p className="p3">
-                Waiting room~ <span>{userName}</span>
-              </p>
+              </Link>
+              <div className="para3">
+                <p className="p1" style={{ color: "#333" }}>
+                  {item.title}
+                </p>
+                <p className="p2">
+                  {new Date(item.eventDate).toLocaleString()}
+                </p>
+                <p className="p3">
+                  Waiting room~ <span>{userName}</span>
+                </p>
 
-              <div className="buttonss">
-                {/* <Link
+                <div className="buttonss">
+                  {/* <Link
               to={
                 "https://ada6251557bb7a455bb6-ahmedhamdy09s-projects.vercel.app/create"
               }
@@ -119,39 +122,40 @@ const UppliveShow1 = () => {
                 </button>
               )}
             </Link> */}
-                {/* {!joined && ( */}
-                {res && res.ownerId && res.ownerId._id === temp._id ? (
-                  <button
-                    type="button"
-                    className="btn btn-info live-btn"
-                    onClick={() => handleJoinHost(res?._id)}>
-                    Join Now
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    className="btn btn-info live-btn"
-                    onClick={() => handleJoinUser(res?._id)}>
-                    Join Now
-                  </button>
-                )}
-                {/* )} */}
-                {/* <button type="button" className="btn btn-secondary remind-btn">
+                  {/* {!joined && ( */}
+                  {res && res.ownerId && res.ownerId._id === temp._id ? (
+                    <button
+                      type="button"
+                      className="btn btn-info live-btn"
+                      onClick={() => handleJoinHost(res?._id)}>
+                      Join Now
+                    </button>
+                  ) : (
+                    <button
+                      type="button"
+                      className="btn btn-info live-btn"
+                      onClick={() => handleJoinUser(res?._id)}>
+                      Join Now
+                    </button>
+                  )}
+                  {/* )} */}
+                  {/* <button type="button" className="btn btn-secondary remind-btn">
                   <img src={bell} alt="reminder Me" />
                   let Me Know
                 </button> */}
 
-                <button type="button" className="btn btn-light invite-btn">
-                  <span onClick={() => copyUrlToClipboard(res?._id)}>
-                    Invite Friends
-                  </span>
-                  <img src={share} alt="share" />
-                </button>
+                  <button type="button" className="btn btn-light invite-btn">
+                    <span onClick={() => copyUrlToClipboard(res?._id)}>
+                      Invite Friends
+                    </span>
+                    <img src={share} alt="share" />
+                  </button>
+                </div>
               </div>
+              <br />
             </div>
-            <br />
-          </div>
-        ))}
+          ) : null
+        )}
     </div>
   );
 };
